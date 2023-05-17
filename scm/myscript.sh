76dbe5a7cfdb1884eb808ae0860577791515261d
Guang Yang <guangyangok@gmail.com>
2023-05-17 18:16:17 +0900

    Update myscript.sh

--- scm/myscript.sh
+++ scm/myscript.sh
@@ -7,7 +7,6 @@ echo '#define IMPLINIT "Init5f3.scm"'>>scmflags.h
 echo '#define COMPILED_INITS init_socket();'>>scmflags.h
 echo '#define CAUTIOUS'>>scmflags.h
 echo '#define CAREFUL_INTS'>>scmflags.h
-echo '#define CAN_DUMP'>>scmflags.h
 # ================ Compile C source files
 cc -O3 -c -g socket.c continue.c scm.c scmmain.c findexec.c script.c time.c repl.c scl.c eval.c sys.c subr.c debug.c unif.c rope.c definedatatype.c ega.c
 # ================ Link C object files
