16d9ce04f7ba784a295018815e42b961162a1440
Guang Yang <guangyangok@gmail.com>
2023-11-10 01:45:11 +0900

    修改build脚本

--- scm/myscript.sh
+++ scm/myscript.sh
@@ -11,4 +11,4 @@ echo '#define CAREFUL_INTS'>>scmflags.h
 cc -O3 -c -g socket.c continue.c scm.c scmmain.c findexec.c script.c time.c repl.c scl.c eval.c sys.c subr.c debug.c unif.c rope.c definedatatype.c ega.c
 # ================ Link C object files
 mv -f scm scm~
-cc -o scm socket.o continue.o scm.o scmmain.o findexec.o script.o time.o repl.o scl.o eval.o sys.o subr.o debug.o unif.o rope.o definedatatype.o ega.o -g -fno-pie
+cc -o scm socket.o continue.o scm.o scmmain.o findexec.o script.o time.o repl.o scl.o eval.o sys.o subr.o debug.o unif.o rope.o definedatatype.o ega.o
